d51bdff381178fd12b209e278b9d93a150f1116f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 21:55:31 +0530

    connecting to database

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+const app=express();
+
+
+app.use(express.json());
+
+
+// connect to the database
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to the database");
+}).catch((error)=>{
+    console.log("error ", error);
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`);
+});
