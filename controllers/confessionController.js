fcfc337af24b3f8b8d0e9322a2a4aeabf73d8e96
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 23:02:47 +0530

    updating confession routes logic

--- controllers/confessionController.js
+++ controllers/confessionController.js
@@ -1,25 +1,62 @@
-
-
+const Confession = require('../models/confessionModel');
+const mongoose = require('mongoose');
 // get confwssion
 
 
 const getAllConfessions = async (req, res) => {
-    console.log("get all confessions");
-    res.send("get all confessions");
+    try {
+        const confessions = await Confession.find();
+        res.status(200).json(confessions);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 // add confession
 
 const addConfession = async (req, res) => {
-    console.log("add confession");
-    res.send("add confession");
+    const { name, description, comments, likes, likedby, reportedby, createdAt } = req.body;
+    let emptyfields = [];
+    if (!name) {
+        emptyfields.push("name");
+    }
+    if (!description) {
+        emptyfields.push("description");
+    }
+
+    if (emptyfields.length > 0) {
+        return res.status(400).json({ message: `please fill in the following fields: ${emptyfields.join(", ")}` });
+    }
+    const newConfession = new Confession({
+        name,
+        description,
+        comments,
+        likes,
+        likedby,
+        reportedby,
+        createdAt
+    });
+    try {
+        await newConfession.save();
+        res.status(201).json({ message: "Confession added successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 // delete confession
 
 const deleteConfession = async (req, res) => {
-    console.log("delete confession");
-    res.send("delete confession");
+    const id = req.params.id;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) throw new Error("Invalid confession id");
+        const confession = await Confession.findById(id);
+        if (!confession) throw new Error("Confession not found");
+        await Confession.findByIdAndDelete(id);
+        res.status(200).json({ message: "Confession deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 module.exports = {
--- routes/confession.js
+++ routes/confession.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const ConfessionControllers = require('../controllers/confessionController');
 // confession route
 
-router.get('/getconfession', ConfessionControllers.getAllConfessions);
+router.get('/confessions', ConfessionControllers.getAllConfessions);
 
 router.post('/addconfession', ConfessionControllers.addConfession);
 
-router.delete('deleteconfession/:id', ConfessionControllers.deleteConfession);
+router.delete('/deleteconfession/:id', ConfessionControllers.deleteConfession);
 
 
 module.exports = router;
