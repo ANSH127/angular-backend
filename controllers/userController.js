e229b53ab9c5a1e129954648efe6338ac505c47b
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 23:19:53 +0530

    updating auth routes logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,31 @@
+const jwt=require('jsonwebtoken')
+const User=require('../models/userModel')
+
+const createToken=(_id)=>{
+    return jwt.sign({_id},process.env.SECRET_KEY,{expiresIn:'3d'})
+}
 
 
 const loginUser= async (req, res) => {
-    console.log("login user");
-    res.send("login user");
+    const {email,password}=req.body
+    try{
+        const user=await User.login(email,password)
+        const token=createToken(user._id)
+        res.status(200).json({email,token})
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
 }
 
 const signupUser = async (req, res) => {
-    console.log("signup user");
-    res.send("signup user");
+    const { email, password } = req.body;
+    try {
+        const user = await User.signup(email, password);
+        const token=createToken(user._id)
+        res.status(201).json({ email, token });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 }
 
 module.exports = {
