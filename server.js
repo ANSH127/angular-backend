ea777a98d06ca5607e8c4928835de730d0d1b335
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 21:59:12 +0530

    adding dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express=require('express');
-const mongoose=require('mongoose');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app=express();
+const app = express();
 
 
 app.use(express.json());
@@ -9,12 +10,12 @@ app.use(express.json());
 
 // connect to the database
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("connected to the database");
-}).catch((error)=>{
+}).catch((error) => {
     console.log("error ", error);
 });
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
 });
