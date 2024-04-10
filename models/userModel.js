b8b6d3dcaa1ce860a11cbc0fa25d09f435c6cc49
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 23:13:26 +0530

    updating user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,70 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
+
 });
 
-const User = mongoose.model('User', userSchema);
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if(!email || !password){
+        throw Error("Email and password are required")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Email is invalid")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough")
+    }
+
+    const exists=await this.findOne({email})
+    if(exists){
+        throw Error("Email already exists")
+    }
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user=await this.create({email,password:hashedPassword})
+    return user
+
+
+}
+
+// static login method
+
+userSchema.statics.login = async function(email, password) {
+    
+        // validation
+        if(!email || !password){
+            throw Error("Email and password are required")
+        }
+        
+        const user=await this.findOne({email})
+        if(!user){
+            throw Error("Email does not exist")
+        }
+        const auth=await bcrypt.compare(password,user.password)
+        if(!auth){
+            throw Error("Password is incorrect")
+        }
+        return user
+
+}
+
+
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
