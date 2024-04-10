2aef67d348eb5f800e7cda2ab851cc2cd1bde2d9
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 22:39:12 +0530

    setting up models

---
+++ models/confessionModel.js
@@ -0,0 +1,39 @@
+
+const mongoose = require('mongoose');
+
+const confessionSchema = new mongoose.Schema({
+    comments: {
+        type: Array,
+
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    likedby: {
+        type: Array
+    },
+    reportedby: {
+        type: Array
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+
+
+});
+
+
+const Confession = mongoose.model('Confession', confessionSchema);
+
+module.exports = Confession;
